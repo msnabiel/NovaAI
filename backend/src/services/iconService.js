023bfb32ebc890d69a56b6503c3a747bcf89ff31
msnabiel <89602212+msnabiel@users.noreply.github.com>
2024-06-30 16:31:47 +0530

    Delete backend/src directory

--- backend/src/services/iconService.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const config = require('../config');
-
-exports.fetchIcons = async () => {
-  const response = await axios.get(`https://api.fontawesome.com/icons?api_key=${config.FONT_AWESOME_API_KEY}`);
-  return response.data;
-};
